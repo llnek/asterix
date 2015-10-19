753f27d0cb6101910450bf662b644d1fb1a6266e
Ken Leung <mailtokenl@gmail.com>
2015-10-19 12:21:37 -0700

    kl

---
+++ src/main/cpp/tictactoe/p/HUD.h
@@ -0,0 +1,63 @@
+// This library is distributed in  the hope that it will be useful but without
+// any  warranty; without  even  the  implied  warranty of  merchantability or
+// fitness for a particular purpose.
+// The use and distribution terms for this software are covered by the Eclipse
+// Public License 1.0  (http://opensource.org/licenses/eclipse-1.0.php)  which
+// can be found in the file epl-v10.html at the root of this distribution.
+// By using this software in any  fashion, you are agreeing to be bound by the
+// terms of this license. You  must not remove this notice, or any other, from
+// this software.
+// Copyright (c) 2013-2015, Ken Leung. All rights reserved.
+
+#if !defined(__HUD_H__)
+#define __HUD_H__
+
+#include "x2d/XHUDLayer.h"
+NS_BEGIN(tttoe)
+
+//////////////////////////////////////////////////////////////////////////
+//
+class CC_DLL HUDLayer : public f::XHUDLayer {
+protected:
+
+  void ShowMenu(c::Ref*);
+  void OnReplay(c::Ref*);
+
+  void InitLabels();
+  void InitIcons();
+
+  void ShowTimer();
+  void UpdateTimer(float);
+  void ShowCountDown(const stdstr& msg);
+  void KillTimer();
+  void UpdateScore(int pcolor,  int value);
+  void Draw(bool running,  int pnum);
+  void EndGame(int winner);
+  void DrawStatusText(obj, const stdstr& msg);
+  void DrawScores();
+  void DrawResult(int pnum);
+  void DrawStatus(int pnum);
+  void RegoPlayers(color1,p1ids,color2,p2ids);
+
+  stdstr p2Long;
+  stdstr p1Long;
+  stdstr p2ID;
+  stdstr p1ID;
+
+public:
+
+  virtual int GetIID() { return 3; }
+  virtual f::XLayer* Realize();
+
+  virtual void ResetAsNew();
+  virtual void Reset();
+
+  NO__CPYASS(HUDLayer)
+  IMPL_CTOR(HUDLayer)
+
+};
+
+
+NS_END(tttoe)
+#endif
+
--- src/main/cpp/tictactoe/p/Menu.cpp
+++ src/main/cpp/tictactoe/p/Menu.cpp
@@ -9,161 +9,153 @@
 // this software.
 // Copyright (c) 2013-2015, Ken Leung. All rights reserved.
 
+#include "core/CCSX.h"
 #include "Menu.h"
 
 NS_BEGIN(tttoe)
 
 //////////////////////////////////////////////////////////////////////////////
-static SEED= {
-  ppids: {},
-  grid: [
-    0,0,0,
-    0,0,0,
-    0,0,0
-  ],
-  size: 3,
-  pnum: 1,
-  mode: 0
-};
-
-//////////////////////////////////////////////////////////////////////////////
-class CC_DLL MainMenuLayer : public f::XLayer {
+class CC_DLL MenuLayer : public f::XLayer {
 public:
-
+  void OnNetPlay(c::Ref*);
+  void OnPlay(c::Ref*);
+  void OnBack(c::Ref*);
+  NO__CPYASS(MenuLayer)
+  IMPL_CTOR(MenuLayer)
 }
 
-f::XLayer* MainMenu::Realize() {
-  auto c= cx::ColorRGB("#f6b17f");
+//////////////////////////////////////////////////////////////////////////
+//
+f::XLayer* MainMenuLayer::Realize() {
+  //auto c= cx::ColorRGB("#f6b17f");
+  auto c= cx::ColorRGB("#5E3178");
   auto wb=cx::VisBox();
   auto cw= cx::Center();
   auto lb= cx::ReifyBmfLabel(cw.x, wb.top * 0.9,
       "font.JellyBelly",
       XCFG()->GetL10NStr("mmenu"));
+
+  CenterImage("gui.mmenu.menu.bg");
+  IncIndexZ();
+
   lb->setColor(c::Color3B(c[0],c[1],c[2]));
   lb->setScale(XCFG()->GetScale());
   AddItem(lb);
 
+  auto menu=  f::ReifyRefType<cocos2d::Menu>();
+  auto b= cx::ReifyMenuBtn("online.png");
+  auto seed= f::ReifyRefType<SeedData>();
+  seed->mode = f::GMode::ONLINE;
+  b->setUserObject(seed);
+  b->setTarget(this,
+      CC_MENU_SELECTOR(MenuLayer::OnNetPlay));
+  menu->addChild(b);
+
+  //p[ sh.l10n('%p1') ] = [ 1, sh.l10n('%player1') ];
+  //p[ sh.l10n('%p2') ] = [ 2, sh.l10n('%player2') ];
+  seed= f::ReifyRefType<SeedData>();
+  seed->mode = f::GMode::TWO;
+  b= cx::ReifyMenuBtn("player2.png");
+  b->setUserObject(seed);
+  b->setTarget(this,
+      CC_MENU_SELECTOR(MenuLayer::OnPlay));
+  menu->addChild(b);
+
+  //p[ sh.l10n('%cpu') ] = [ 2, sh.l10n('%computer') ];
+  //p[ sh.l10n('%p1') ] = [ 1,  sh.l10n('%player1') ];
+  seed= f::ReifyRefType<SeedData>();
+  seed->mode = f::GMode::ONE;
+  b= cx::ReifyMenuBtn("player1.png");
+  b->setUserObject(seed);
+  b->setTarget(this,
+      CC_MENU_SELECTOR(MenuLayer::OnPlay));
+  menu->addChild(b);
+  menu->setPosition(cw);
+  AddItem(menu);
+
+  // back-quit button
+  auto b= cx::ReifyMenuBtn("icon_back.png");
+  b->setTarget(this,
+      CC_MENU_SELECTOR(MenuLayer::OnBack));
+  b->setColor(c::Color3B(c[0],c[1],c[2]));
+  auto q= cx::ReifyMenuBtn("icon_quit.png");
+  q->setTarget(this,
+      CC_MENU_SELECTOR(XLayer::OnQuit));
+  q->setColor(c::Color3B(c[0],c[1],c[2]));
+  auto m2= cx::MkBackQuit(b, q, false);
+  auto sz= b->getContentSize();
+
+  m2->setPosition(wb.left + tile + sz.width * 1.1,
+                  wb.bottom + tile + sz.height * 0.45);
+  AddItem(m2);
+
+  // audio
+  c::MenuItem* off;
+  c::MenuItem* on;
+  cx::ReifyAudioIcons(on,off);
+  off->setColor(c::Color3B(c[0],c[1],c[2]));
+  on->setColor(c::Color3B(c[0],c[1],c[2]));
+
+  AddAudioIcons(off, on,
+      cx::AnchorBR(),
+      c::Vec2(wb.right - tile, wb.bottom + tile));
+
+  return this;
 }
-  /**
-   * @method onnetplay
-   * @private
-   */
-  onnetplay(msg) {
-    const gl= sh.protos[sh.ptypes.game],
-    ol= sh.protos[sh.ptypes.online],
-    mm= sh.protos[sh.ptypes.mmenu];
-    msg.onback= () => { ccsx.runScene( mm.reify()); };
-    msg.yes= (wss,pnum,startmsg) => {
-      const m= sjs.mergeEx(R.omit(['yes',
-                                   'onback'], msg), {
-        wsock: wss,
-        pnum: pnum
-      });
-      sjs.merge(m, startmsg);
-      ccsx.runScene( gl.reify(m));
-    }
-    ccsx.runScene(ol.reify(msg));
-  },
-  /**
-   * @method onplay
-   * @private
-   */
-  onplay(msg) {
-    ccsx.runScene(sh.protos[sh.ptypes.game].reify(msg));
-  },
-  /**
-   * @method setup
-   * @protected
-   */
-  setup() {
-    this.centerImage(sh.getImage('gui.mmenu.menu.bg'));
-    this.incIndexZ();
-    this.title();
-    const color= cc.color('#5E3178'),
-    cw = ccsx.center(),
-    wb= ccsx.vbox(),
-    me=this,
-    p={},
-    menu= ccsx.vmenu([
-      { nnn: '#online.png',
-        cb() {
-          me.onnetplay(sjs.mergeEx(SEED,
-                                   { mode: sh.gtypes.ONLINE_GAME}));
-        }},
-      { nnn: '#player2.png',
-        cb() {
-          p[ sh.l10n('%p1') ] = [ 1, sh.l10n('%player1') ];
-          p[ sh.l10n('%p2') ] = [ 2, sh.l10n('%player2') ];
-          me.onplay(sjs.mergeEx(SEED,
-                                {ppids: p,
-                                 mode: sh.gtypes.P2_GAME }));
-        }},
-      { nnn: '#player1.png',
-        cb() {
-          p[ sh.l10n('%cpu') ] = [ 2, sh.l10n('%computer') ];
-          p[ sh.l10n('%p1') ] = [ 1,  sh.l10n('%player1') ];
-          me.onplay(sjs.mergeEx(SEED,
-                                {ppids: p,
-                                 mode: sh.gtypes.P1_GAME }));
-        }}
-    ],
-    { pos: cw });
-    this.addItem(menu);
 
-    this.mkBackQuit(false, [{
-        nnn: '#icon_back.png',
-        color: color,
-        cb() {
-          me.options.onback();
-        }},
-      { nnn: '#icon_quit.png',
-        color: color,
-        cb() { me.onQuit(); }
-      }],
-      (m,z) => {
-        m.setPosition(wb.left + csts.TILE + z.width * 1.1,
-                      wb.bottom + csts.TILE + z.height * 0.45);
-    });
+//////////////////////////////////////////////////////////////////////////
+//
+void MenuLayer::OnNetPlay(c::Ref* r) {
+  auto ol = f::ReifyRefType<Online>();
+  auto y= []() { cx::RunScene(MainGame::Reify()); }
+  auto yes= c::CallFunc::create(y);
+  auto n= []() { cx::RunScene(MainMenu::Reify()); }
+  auto no= c::CallFunc::create(n);
+  auto s= Online::Reify(ol, yes,no);
+  cx::RunScene(s);
+}
 
-    this.mkAudio({
-      pos: cc.p(wb.right - csts.TILE,
-                wb.bottom + csts.TILE),
-      color: color,
-      anchor: ccsx.acs.BottomRight
-    });
-  },
-  /**
-   * @method ctor
-   * @private
-   */
-  ctor(options) {
-    this._super(options);
-  }
-});
+//////////////////////////////////////////////////////////////////////////
+//
+void MenuLayer::OnPlay(c::Ref* r) {
+  cx::RunScene( MainGame::Reify() );
+}
 
-/** @alias module:p/mmenu */
-const xbox= /** @lends xbox# */{
-  /**
-   * @property {String} rtti
-   */
-  rtti: sh.ptypes.mmenu,
-  /**
-   * Create the Main Menu screen.
-   * @method reify
-   * @param {Object} options
-   * @return {cc.Scene}
-   */
-  reify(options) {
-    return new scenes.XSceneFactory([
-      MainMenuLayer
-    ]).reify(options);
-  }
-};
-
-sjs.merge(exports, xbox);
-/*@@
-return xbox;
-@@*/
 //////////////////////////////////////////////////////////////////////////////
-//EOF
+//
+MainMenu* MainMenu::ReifyWithBackAction(c::CallFunc* back) {
+  auto m = f::ReifyRefType<MainMenu>();
+  m->backAction= back;
+  back->retain();
+  m->Realize();
+  return m;
+}
 
+//////////////////////////////////////////////////////////////////////////////
+//
+void MainMenu::OnBackAction() {
+  backAction->execute();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+f::XScene* MainMenu::Realize() {
+  auto y = f::ReifyRefType<MenuLayer>();
+  AddLayer(y);
+  y->Realize();
+  return this;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+MainMenu::~MainMenu() {
+  if (NNP(backAction)) { backAction->release(); }
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+MainMenu::MainMenu() {
+  SNPTR(backAction)
+}
+
+NS_END(tttoe)
