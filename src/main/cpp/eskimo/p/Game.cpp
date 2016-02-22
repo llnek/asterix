9f2069c8be6bdde6eb40f59b8401f1e89c58b1cf
Ken Leung <mailtokenl@gmail.com>
2016-02-22 14:34:21 -0800

    kl

--- src/main/cpp/eskimo/p/Game.cpp
+++ src/main/cpp/eskimo/p/Game.cpp
@@ -43,6 +43,95 @@ void GLayer::decorate() {
   engine = mc_new(GEngine);
 }
 
+//////////////////////////////////////////////////////////////////////////////
+//
+void GLayer::createScreen() {
+  auto wb= cx::visBox();
+
+  centerImage("game.bg");
+  regoAtlas("game-pics", kMiddleground);
+
+    _igloo = Igloo::create(this);
+    _igloo->setVisible(false);
+    _gameBatchNode->addChild(_igloo, kForeground);
+
+    //create buttons
+    _buttons = Vector<Sprite *>(5);
+
+    _btnStart = Sprite::createWithSpriteFrameName("btn_start_off.png");
+    _btnStart->setPosition(Vec2(_screenSize.width * 0.5f, _screenSize.height * 0.45f));
+    _btnStart->setVisible(false);
+    _gameBatchNode->addChild(_btnStart, kForeground, kSpriteBtnStart);
+    auto buttonOn = Sprite::createWithSpriteFrameName("btn_start_on.png");
+    buttonOn->setAnchorPoint(Vec2(0,0));
+    buttonOn->setVisible(false);
+    _btnStart->addChild(buttonOn, kForeground, kSpriteBtnOn);
+    _buttons.pushBack(_btnStart);
+
+    _btnReset = Sprite::createWithSpriteFrameName("btn_reset_off.png");
+    _btnReset->setAnchorPoint(Vec2(0.5, 1));
+    _btnReset->setPosition(Vec2(_screenSize.width * 0.9f, _screenSize.height));
+    _btnReset->setVisible(false);
+    _gameBatchNode->addChild(_btnReset, kForeground, kSpriteBtnReset);
+    buttonOn = Sprite::createWithSpriteFrameName("btn_reset_on.png");
+    buttonOn->setAnchorPoint(Vec2(0,0));
+    buttonOn->setVisible(false);
+    _btnReset->addChild(buttonOn, kForeground, kSpriteBtnOn);
+    _buttons.pushBack(_btnReset);
+
+    _btnPause = Sprite::createWithSpriteFrameName("btn_pause_off.png");
+    _btnPause->setAnchorPoint(Vec2(0.5, 1));
+    _btnPause->setPosition(Vec2(_screenSize.width * 0.1f, _screenSize.height));
+    _btnPause->setVisible(false);
+    _gameBatchNode->addChild(_btnPause, kForeground, kSpriteBtnPause);
+    buttonOn = Sprite::createWithSpriteFrameName("btn_pause_on.png");
+    buttonOn->setAnchorPoint(Vec2(0,0));
+    buttonOn->setVisible(false);
+    _btnPause->addChild(buttonOn, kForeground, kSpriteBtnOn);
+    _buttons.pushBack(_btnPause);
+
+    _btnAgain = Sprite::createWithSpriteFrameName("btn_again_off.png");
+    _btnAgain->setPosition(Vec2(_screenSize.width * 0.5f, _screenSize.height * 0.45f));
+    _btnAgain->setVisible(false);
+    _gameBatchNode->addChild(_btnAgain, kForeground, kSpriteBtnAgain);
+    buttonOn = Sprite::createWithSpriteFrameName("btn_again_on.png");
+    buttonOn->setAnchorPoint(Vec2(0,0));
+    buttonOn->setVisible(false);
+    _btnAgain->addChild(buttonOn, kForeground, kSpriteBtnOn);
+    _buttons.pushBack(_btnAgain);
+
+    _btnMenu = Sprite::createWithSpriteFrameName("btn_menu_off.png");
+    _btnMenu->setPosition(Vec2(_screenSize.width * 0.5f, _screenSize.height * 0.3f));
+    _btnMenu->setVisible(false);
+    _gameBatchNode->addChild(_btnMenu, kForeground, kSpriteBtnMenu);
+    buttonOn = Sprite::createWithSpriteFrameName("btn_menu_on.png");
+    buttonOn->setAnchorPoint(Vec2(0,0));
+    buttonOn->setVisible(false);
+    _btnMenu->addChild(buttonOn, kForeground, kSpriteBtnOn);
+    _buttons.pushBack(_btnMenu);
+
+    _messages = Label::createWithBMFont("font_messages.fnt", "level", TextHAlignment::CENTER);
+    _messages->setAnchorPoint(Vec2(0.5f, 0.5f));
+    _messages->setVisible(false);
+    _messages->setPosition(Vec2(_screenSize.width * 0.5f, _screenSize.height * 0.6f));
+    this->addChild(_messages, kForeground);
+
+    _smoke = ParticleSystemQuad::create("smoke.plist");
+    _smoke->setVisible(false);
+    _smoke->stopSystem();
+    this->addChild(_smoke, kForeground);
+
+
+    _tutorialLabel = Label::createWithTTF(TUTORIAL_1, "fonts/Verdana.ttf", 25);
+    _tutorialLabel->setWidth(_screenSize.width * 0.7f);
+    _tutorialLabel->setHeight(_screenSize.height * 0.4f);
+    _tutorialLabel->setAlignment(TextHAlignment::CENTER);
+    _tutorialLabel->setPosition(Vec2 (_screenSize.width * 0.5f, _screenSize.height * 0.2f) );
+    this->addChild(_tutorialLabel, kForeground);
+    _tutorialLabel->setVisible(false);
+}
+
+
 END_NS_UNAMED
 //////////////////////////////////////////////////////////////////////////////
 //
---
+++ src/main/cpp/eskimo/p/Igloo.cpp
@@ -0,0 +1,83 @@
+// This library is distributed in  the hope that it will be useful but without
+// any  warranty; without  even  the  implied  warranty of  merchantability or
+// fitness for a particular purpose.
+// The use and distribution terms for this software are covered by the Eclipse
+// Public License 1.0  (http://opensource.org/licenses/eclipse-1.0.php)  which
+// can be found in the file epl-v10.html at the root of this distribution.
+// By using this software in any  fashion, you are agreeing to be bound by the
+// terms of this license. You  must not remove this notice, or any other, from
+// this software.
+// Copyright (c) 2013-2016, Ken Leung. All rights reserved.
+
+#include "core/XConfig.h"
+#include "core/CCSX.h"
+#include "Igloo.h"
+NS_ALIAS(cx,fusii::ccsx)
+NS_BEGIN(eskimo)
+
+//////////////////////////////////////////////////////////////////////////////
+//
+Igloo::~Igloo() {
+  EVENT_DISPATCHER->removeCustomEventListeners(NOTIFICATION_GRAVITY_SWITCH);
+  EVENT_DISPATCHER->removeCustomEventListeners(NOTIFICATION_LEVEL_COMPLETED);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+Igloo::Igloo(GVars *ss) {
+
+  auto onGravityChanged = [=](EventCustom*) {
+    _block->setSpriteFrame(
+        cx::getSpriteFrame("block_large_" +
+          s::to_string( (int) ss->gravity) + ".png"));
+  };
+
+  auto onLevelCompleted = [=](EventCustom*) {
+    this->setDisplayFrame(cx::getSpriteFrame("igloo_on.png"));
+  };
+
+    //register game notifications
+  ADD_NOTIFICATION(this, NOTIFICATION_GRAVITY_SWITCH, onGravityChanged);
+  ADD_NOTIFICATION(this, NOTIFICATION_LEVEL_COMPLETED, onLevelCompleted);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+Igloo* Igloo::create(GVars *ss) {
+  auto sprite = mc_new1(Igloo, ss);
+  if (sprite->initWithSpriteFrameName("igloo_off.png")) {
+    sprite->autorelease();
+    sprite->addBlock();
+    return sprite;
+  }
+  CC_SAFE_DELETE(sprite);
+  return CC_NIL;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+void Igloo::initIgloo(int gravity, const c::Vec2 &pos) {
+
+  _block->setDisplayFrame(
+      cx::getSpriteFrame(
+        "block_large_" + s::to_string(ss->gravity) + ".png"));
+
+  this->setPosition(pos);
+  this->setDisplayFrame(cx::getSpriteFrame("igloo_off.png"));
+  setVisible(true);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+void Igloo::addBlock () {
+  _block = cx::reifySprite("block_large_1.png");
+  _block->setAnchorPoint(cx::anchorBL());
+  _block->setPosition(0, -TILE * 0.75f);
+  addChild(_block, kBackground);
+}
+
+
+
+NS_END
+
+
